d00fb061a767c33357a8359b5a6169deada0932e
manbhav234 <manbhav234@gmail.com>
2025-06-30 14:04:57 +0530

    auth setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,18 @@
 import express from "express"
-
+import passport from 'passport';
+import './utils/passportStrategy'
+import authRouter from "./routes/auth";
+import cookieSession from 'cookie-session'
 const app = express();
 
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [`${process.env.SESSION_COOKIE_KEY}`]
+}))
 
+app.use(passport.initialize());
+app.use(passport.session())
+
+app.use("/api/v1/auth", authRouter);
 
 export default app;
---
+++ server/src/utils/passportStrategy.ts
@@ -0,0 +1,53 @@
+import passport from 'passport'
+import errorHandler from './errorHandler'
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'
+import prisma from "../db/client"
+import { User } from '@prisma/client'
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+})
+
+passport.deserializeUser((user: Express.User, done) => {
+    done(null, user as User)
+})
+
+async function userSignUp(name:string, email: string){
+    const newUser: User = await prisma.user.create({
+        data: {
+            name: name,
+            email: email
+        }
+    })
+    return newUser;
+}
+
+async function userLogin(name: string, email: string){
+    const userDetails: User | null = await prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    })
+    if (!userDetails){
+        const newUser: User = await userSignUp(name, email);
+        return newUser;
+    }
+    return userDetails;
+}
+
+
+const strategy = new GoogleStrategy({
+    clientID: process.env.CLIENT_ID ?? errorHandler("Missing CLIENT_ID environment variable"),
+    clientSecret: process.env.CLIENT_SECRET ?? errorHandler("Missing CLIENT_SECRET environment variable"),
+    callbackURL: process.env.REDIRECT_URI  
+    //FIXME - get proper types for function parameters
+}, async (accessToken: any, refreshToken: any, profile: any, details: any, done: any) => {
+    try{
+        const user: User = await userLogin(details._json.name, details._json.email)
+        done(null, user)
+    }catch(e){
+        console.log("Error occured: ", e)
+    }
+})
+
+passport.use(strategy);
