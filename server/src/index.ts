afb9d4233dd338078c9f9a5490ae577e04b1cc9b
manbhav234 <manbhav234@gmail.com>
2025-07-08 20:45:25 +0530

    update server config

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ const port = process.env.PORT;
 const startServer = async () => {
     try {
         app.listen(port, () => {
-            console.log("server started on port: ", port);
+            console.log("Server started on port: ", port);
         })
     }catch(error){
         console.log("Error Starting the Server: ", error)
