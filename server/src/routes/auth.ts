2bfccd351e5ceb1ce3fd569cd860c74685f9e35b
manbhav234 <manbhav234@gmail.com>
2025-07-09 13:55:07 +0530

    testing cookie issue

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -23,6 +23,7 @@ router.get('/google/redirect', passport.authenticate('google', {
     failureRedirect: `${process.env.CLIENT_URL}`,
     failureMessage: true // Passes the message to req.session
   }), (req,res)=>{
+    req.session!.user = req.user;
     res.redirect(`${process.env.CLIENT_URL}`)
 })
 
