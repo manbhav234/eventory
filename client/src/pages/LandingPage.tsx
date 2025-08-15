af8cfab9454467b407a0d2de23a8c4424b1ba320
manbhav234 <manbhav234@gmail.com>
2025-08-15 15:27:47 +0530

    remove dummy buttons from landing page

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -5,6 +5,7 @@ import useAppStore from "../store/mainStore";
 import { useNavigate } from "react-router-dom";
 import Loader from "@/components/Loader";
 import FullScreenLoaderWrapper from "@/components/FullScreenLoaderWrapper";
+import LoginBtn from "@/components/LoginBtn";
 
 const LandingPage = () => {
   const { checkLogin, isLoggedIn, isLoading } = useAppStore();
@@ -87,12 +88,7 @@ const LandingPage = () => {
           }}
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
-          <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-            Explore Now
-          </button>
-          <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
-            Contact Support
-          </button>
+          <LoginBtn/>
         </motion.div>
 
       </div>
